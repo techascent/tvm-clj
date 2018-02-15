4992e97ff5c1f2c0c764bbcd8bfb0f401c8638df
Chris Nuernberger <chris@thinktopic.com>
2018-02-15 13:43:54 -0700

    Updated project to use core

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [ml.dmlc.tvm/tvm4j-core "0.0.1-SNAPSHOT"]])
