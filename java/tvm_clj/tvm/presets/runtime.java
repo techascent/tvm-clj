979cc831c2c7b2ee80a52f2f9b7d6ed13f3ad254
Chris Nuernberger <chris@thinktopic.com>
2018-03-02 09:54:02 -0700

    Updated tvm and fixed a small build issue.

--- java/tvm_clj/tvm/presets/runtime.java
+++ java/tvm_clj/tvm/presets/runtime.java
@@ -11,6 +11,7 @@ import org.bytedeco.javacpp.tools.*;
 public class runtime implements InfoMapper {
   public void map(InfoMap infoMap) {
     infoMap.put(new Info("__cplusplus","_WIN32").define(false))
-      .put(new Info("TVM_DLL").cppTypes().annotations().cppText(""));
+      .put(new Info("TVM_DLL").cppTypes().annotations().cppText(""))
+      .put(new Info("TVM_WEAK").cppTypes().annotations().cppText(""));
   }
 }
