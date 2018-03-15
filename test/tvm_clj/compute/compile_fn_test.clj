3c46e5c8d5ca32b825b7cefe28497324fecb61e7
Chris Nuernberger <chris@thinktopic.com>
2018-03-15 09:01:04 -0600

    Finalizing target functionality.

--- test/tvm_clj/compute/compile_fn_test.clj
+++ test/tvm_clj/compute/compile_fn_test.clj
@@ -8,32 +8,34 @@
   "Input is unsigned bytes, so values 0->255, bgr [height width 3].
 Output: {:datatype :float32 :shape [3 height width]}, values from -0.5->0.5"
   [input-tensor]
-  (let [result (ct/new-tensor  (concat [3] (take 2 (ct/shape input-tensor)))
-                               :datatype :float32)
-        reshape-indexes (ct/->tensor [2 1 0]
-                                     :datatype :int32
-                                     :allocation :const)
-        input-reshaped (-> (ct/select :all :all reshape-indexes)
-                           (ct/transpose [2 0 1]))]
-    (-> (ct/=! result input-reshaped)
-        (ct//! 255)
-        (ct/+! -0.5))))
+  ;;Move to bgr instead of rgb  Also drop alpha if exists in first place.
+  (-> (ct/select :all :all [2 1 0])
+      ;;transpose to channels first.
+      (ct/transpose [2 0 1])
+      ;;First actual operation that is compiled.
+      (ct/convert :float32)
+      ;;Rest of the work.  These should all get rolled into assignment step above.
+      (ct// 255)
+      (ct/- 0.5)))
+
 
 (defn convert-floats-to-bgr-bytes
   "Converts to bgr image"
   [img-tensor]
-  (let [result (ct/new-tensor (concat (drop 1 (ct/shape input-tensor))
-                                      [(last input-tensor)]))
-        reshape-indexes (ct/->tensor [2 1 0]
-                                     :datatype :int32
-                                     :allocation :const)
-        img-tensor (ct/select reshape-indexes reshape-indexes :all :all)]
-    (-> (ct/+! ))
-    )
-  )
+  (-> (ct/+ 0.5)
+      (ct/* 255.0)
+      (ct/clamp 0 255)
+      ;;rgb -> bgr
+      (ct/select [2 1 0] :all :all)
+      ;;Move channels to last from planar channels first
+      (ct/transpose [1 2 0])
+      ;;Last operation, uses sophisticated indexing + transposition
+      (ct/convert :uint8)))
+
 
 (def cpu-compiled-image->floats (tvm/compile-cpu! convert-image-to-floats
                                                   (ct/bind-tensor [img-height img-width 4])))
+
 
 (let [test-image (-> (opencv/imload (io/resource "test-image.png"))
                      ;;Zero memcopy; uses opencv mat pointer
