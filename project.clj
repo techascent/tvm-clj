1ddd50abe935db8b6c81036647a750e8b2a81716
Chris Nuernberger <chris@thinktopic.com>
2018-10-08 13:37:13 -0600

    Unit test fixes after upgrades.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject tvm-clj "0.1.2-SNAPSHOT"
+(defproject tvm-clj "0.1.2"
   :description "Clojure bindings and exploration of the tvm library"
   :url "http://github.com/tech-ascent/tvm-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [techascent/tech.compute "0.6.0"]
-                 [techascent/tech.javacpp-datatype "0.3.6"]
+                 [techascent/tech.javacpp-datatype "0.3.7"]
                  [potemkin "0.4.4"]]
 
   :profiles {:dev
