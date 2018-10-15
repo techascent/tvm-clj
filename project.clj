b14bd9fc3e61af67702e17c1eccfe7f01651e9a6
Chris Nuernberger <chris@thinktopic.com>
2018-10-15 13:46:30 -0600

    0.1.6

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject tvm-clj "0.1.6-SNAPSHOT"
+(defproject tvm-clj "0.1.6"
   :description "Clojure bindings and exploration of the tvm library"
   :url "http://github.com/tech-ascent/tvm-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [techascent/tech.compute "0.6.8"]
-                 [techascent/tech.javacpp-datatype "0.5.7"]
+                 [techascent/tech.javacpp-datatype "0.5.8"]
                  [potemkin "0.4.4"]]
 
   :profiles {:dev
              ;;Unit tests need this.
-             {:dependencies [[techascent/tech.opencv "0.2.6"]]}}
+             {:dependencies [[techascent/tech.opencv "0.2.7"]]}}
 
   :java-source-paths ["java"]
   :native-path "java/native/"
