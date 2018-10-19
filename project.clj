222307f4346327e000d1c95ea63a49184de3e305
Chris Nuernberger <chris@thinktopic.com>
2018-10-19 10:51:42 -0600

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tvm-clj "0.1.9"
+(defproject tvm-clj "0.1.10-SNAPSHOT"
   :description "Clojure bindings and exploration of the tvm library"
   :url "http://github.com/tech-ascent/tvm-clj"
   :license {:name "Eclipse Public License"
