f757939ef72af49f64232babe043cdfd07db9b7c
Chris Nuernberger <chris@techascent.com>
2018-11-01 16:06:22 -0600

    snap

---
+++ example/README.md
@@ -0,0 +1,11 @@
+# Example project
+
+If you don't want to build the tvm library yourself (which we recommend)
+you can run
+
+```bash
+scripts/docker-repl.sh
+```
+
+And then require tvm from the repl.  A compatible native library should be 
+unpacked and loaded.
--- example/project.clj
+++ example/project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [tvm-clj "1.5-SNAPSHOT"]
+                 [tvm-clj "2.0"]
                  [techascent/tech.opencv "1.5"]])
