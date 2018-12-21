e2bc221ba6e8fcd539d4128ce033930bd14c7969
Chris Nuernberger <chris@thinktopic.com>
2018-12-21 10:05:27 -0700

    updated to latest compute for parallel lib upgrade.

--- project.clj
+++ project.clj
@@ -4,8 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [techascent/tech.compute "3.0"]
+                 [techascent/tech.compute "3.1"]
                  [potemkin "0.4.4"]]
+
+  :plugins [[walmartlabs/vizdeps "0.1.6"]]
 
   :profiles {:dev
              ;;Unit tests need this.
