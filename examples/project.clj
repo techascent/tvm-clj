58f1253fdf84e7c6dcf0133c52ce5b824c655e8a
Chris Nuernberger <chris@techascent.com>
2019-10-20 17:36:11 -0600

    update examples.

--- examples/project.clj
+++ examples/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [tvm-clj "4.7-SNAPSHOT"]
+                 [tvm-clj "5.0"]
                  [techascent/tech.opencv "4.25"]]
   ;;This is useful if you want to see where the loaded tvm library
   ;;is coming from.  We really recommend that you install a tvm
