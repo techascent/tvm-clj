3ab2a5d021796c2b26bc36a138ea57ff1bce529f
Chris Nuernberger <chris@techascent.com>
2019-11-10 14:33:19 -0700

    upgrading the things.

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [techascent/tech.compute "4.42"]
+                 [techascent/tech.compute "4.44"]
                  [potemkin "0.4.4"]]
 
   :profiles {:dev
              ;;Unit tests need this.
-             {:dependencies [[techascent/tech.opencv "4.42"]]}}
+             {:dependencies [[techascent/tech.opencv "4.44"]]}}
 
   :java-source-paths ["java"]
   :native-path "java/native"
