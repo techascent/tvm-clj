12b9a0783d759fa2fb2b0f716b4275fc33cc2cb2
Chris Nuernberger <chris@techascent.com>
2020-11-09 12:48:29 -0700

    Added in cuda,opencl specific bindings.

--- src/tvm_clj/impl/fns/device_api.clj
+++ src/tvm_clj/impl/fns/device_api.clj
@@ -8,6 +8,27 @@
  (with-bindings {#'base/fn-name "device_api.cpu"}
    (apply base/call-function @cpu-fnptr* args)))
 
+(defonce ^:private cpu_pinned-fnptr* (delay (base/name->global-function "device_api.cpu_pinned")))
+(defn cpu_pinned
+ "TVM PackedFn"
+ [& args]
+ (with-bindings {#'base/fn-name "device_api.cpu_pinned"}
+   (apply base/call-function @cpu_pinned-fnptr* args)))
+
+(defonce ^:private gpu-fnptr* (delay (base/name->global-function "device_api.gpu")))
+(defn gpu
+ "TVM PackedFn"
+ [& args]
+ (with-bindings {#'base/fn-name "device_api.gpu"}
+   (apply base/call-function @gpu-fnptr* args)))
+
+(defonce ^:private opencl-fnptr* (delay (base/name->global-function "device_api.opencl")))
+(defn opencl
+ "TVM PackedFn"
+ [& args]
+ (with-bindings {#'base/fn-name "device_api.opencl"}
+   (apply base/call-function @opencl-fnptr* args)))
+
 (defonce ^:private rpc-fnptr* (delay (base/name->global-function "device_api.rpc")))
 (defn rpc
  "TVM PackedFn"
--- src/tvm_clj/impl/fns/runtime/module.clj
+++ src/tvm_clj/impl/fns/runtime/module.clj
@@ -8,6 +8,13 @@
  (with-bindings {#'base/fn-name "runtime.module.loadbinary_GraphRuntimeFactory"}
    (apply base/call-function @loadbinary_GraphRuntimeFactory-fnptr* args)))
 
+(defonce ^:private loadbinary_cuda-fnptr* (delay (base/name->global-function "runtime.module.loadbinary_cuda")))
+(defn loadbinary_cuda
+ "TVM PackedFn"
+ [& args]
+ (with-bindings {#'base/fn-name "runtime.module.loadbinary_cuda"}
+   (apply base/call-function @loadbinary_cuda-fnptr* args)))
+
 (defonce ^:private loadbinary_metadata-fnptr* (delay (base/name->global-function "runtime.module.loadbinary_metadata")))
 (defn loadbinary_metadata
  "TVM PackedFn"
@@ -15,12 +22,47 @@
  (with-bindings {#'base/fn-name "runtime.module.loadbinary_metadata"}
    (apply base/call-function @loadbinary_metadata-fnptr* args)))
 
+(defonce ^:private loadbinary_opencl-fnptr* (delay (base/name->global-function "runtime.module.loadbinary_opencl")))
+(defn loadbinary_opencl
+ "TVM PackedFn"
+ [& args]
+ (with-bindings {#'base/fn-name "runtime.module.loadbinary_opencl"}
+   (apply base/call-function @loadbinary_opencl-fnptr* args)))
+
+(defonce ^:private loadfile_cl-fnptr* (delay (base/name->global-function "runtime.module.loadfile_cl")))
+(defn loadfile_cl
+ "TVM PackedFn"
+ [& args]
+ (with-bindings {#'base/fn-name "runtime.module.loadfile_cl"}
+   (apply base/call-function @loadfile_cl-fnptr* args)))
+
+(defonce ^:private loadfile_clbin-fnptr* (delay (base/name->global-function "runtime.module.loadfile_clbin")))
+(defn loadfile_clbin
+ "TVM PackedFn"
+ [& args]
+ (with-bindings {#'base/fn-name "runtime.module.loadfile_clbin"}
+   (apply base/call-function @loadfile_clbin-fnptr* args)))
+
+(defonce ^:private loadfile_cubin-fnptr* (delay (base/name->global-function "runtime.module.loadfile_cubin")))
+(defn loadfile_cubin
+ "TVM PackedFn"
+ [& args]
+ (with-bindings {#'base/fn-name "runtime.module.loadfile_cubin"}
+   (apply base/call-function @loadfile_cubin-fnptr* args)))
+
 (defonce ^:private loadfile_ll-fnptr* (delay (base/name->global-function "runtime.module.loadfile_ll")))
 (defn loadfile_ll
  "TVM PackedFn"
  [& args]
  (with-bindings {#'base/fn-name "runtime.module.loadfile_ll"}
    (apply base/call-function @loadfile_ll-fnptr* args)))
+
+(defonce ^:private loadfile_ptx-fnptr* (delay (base/name->global-function "runtime.module.loadfile_ptx")))
+(defn loadfile_ptx
+ "TVM PackedFn"
+ [& args]
+ (with-bindings {#'base/fn-name "runtime.module.loadfile_ptx"}
+   (apply base/call-function @loadfile_ptx-fnptr* args)))
 
 (defonce ^:private loadfile_so-fnptr* (delay (base/name->global-function "runtime.module.loadfile_so")))
 (defn loadfile_so
