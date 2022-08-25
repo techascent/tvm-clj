74dba939cddd398f8590a2b37f9597b67e12b0cb
Matthew <matthew@obrizum.com>
2022-08-25 09:21:57 +0100

    update impl namespaces

---
+++ src/tvm_clj/impl/fns/meta_schedule/winograd_data_pack/nchw.clj
@@ -0,0 +1,4 @@
+(ns tvm-clj.impl.fns.meta_schedule.winograd_data_pack.nchw
+  (:require [tvm-clj.impl.tvm-ns-fns :as tvm-ns-fns]))
+
+(tvm-ns-fns/export-tvm-functions "meta_schedule.winograd_data_pack.nchw")
---
+++ src/tvm_clj/impl/fns/script/ir_builder.clj
@@ -0,0 +1,4 @@
+(ns tvm-clj.impl.fns.script.ir_builder
+  (:require [tvm-clj.impl.tvm-ns-fns :as tvm-ns-fns]))
+
+(tvm-ns-fns/export-tvm-functions "script.ir_builder")
